aa58cf36fb51b4c28da6e15f6f728d7e026b3b06
jannesbrunner <jannes.brunner@student.htw-berlin.de>
2019-01-04 17:38:32 +0100

    First version of numpad

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
+from kivy.properties import (StringProperty, 
+    NumericProperty, ObjectProperty)
 
 class CellphoneApp(App):
     def build(self):
@@ -8,7 +10,48 @@ class CellphoneApp(App):
 
 
 class Cellphone(Widget):
-    pass
+    display_text = StringProperty("0")
+    display_value = NumericProperty(0)
+    init_value = NumericProperty(100)
+    maximum_value = NumericProperty(None, allownone=True)
+    minimum_value = NumericProperty(None, allownone=True)
+    return_callback = ObjectProperty(None, allownone=True)
+    units = StringProperty(None, allownone=True)
+
+    def __init__(self, **kwargs):
+        super(Cellphone, self).__init__(**kwargs)
+
+    def check_minimum_value(self):
+        if self.minimum_value != None:
+            if self.display_value < self.minimum_value:
+                self.display_text = str(self.minimum_value)
+
+    def button_callback(self, button_str):
+        if button_str in [str(x) for x in range(10)]:
+            if self.display_text == '0':
+                self.display_text = button_str 
+            else:
+                self.display_text = self.display_text + button_str
+            maximum_value = self.maximum_value
+            if maximum_value != None:
+                if self.display_value > maximum_value: 
+                    self.display_value = maximum_value
+        elif button_str == 'del':
+            self.display_text = self.display_text[:-1]
+        elif button_str == 'ret':
+            self.check_minimum_value()
+            self.return_callback(self.display_value, True)
+
+    def on_display_text(self, instance, value):
+        if value == '':
+            self.display_text = '0'
+            return
+        if int(value) > self.maximum_value and self.maximum_value != None:
+            self.display_text = str(self.maximum_value)
+            return
+        self.display_value = int(value)
+        if self.return_callback is not None:
+            self.return_callback(self.display_value, False)
 
 if __name__ == '__main__':
     CellphoneApp().run()
