b8878f7b28aa2477e557402b99bce8aee7755404
jannesbrunner <jannes.brunner@student.htw-berlin.de>
2019-01-07 16:28:18 +0100

    DialPopup with callbacks

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.label import Label
 from kivy.uix.button import Button
+from kivy.uix.popup import Popup
 from kivy.properties import (StringProperty, 
     NumericProperty, ObjectProperty)
+
+class Storage():
+    number = "325"
 
 class CellphoneApp(App):
     def build(self):
         return Cellphone()
 
+class DialPopup(Popup):
+    display_text = StringProperty("0")
+    
+    def __init__(self, number, **kwargs): # note the `abc`
+        super(DialPopup, self).__init__()
+        self.display_text = number
+
+   
 
 class Cellphone(Widget):
+    
     display_text = StringProperty("0")
     display_value = NumericProperty(0)
     init_value = NumericProperty(100)
@@ -22,8 +36,15 @@ class Cellphone(Widget):
     def __init__(self, **kwargs):
         super(Cellphone, self).__init__(**kwargs)
        
-       
+    def start_call(self, instance):
+        Storage.number = self.display_text
+        print("Call Started")
+        
 
+
+    def end_call(self, instance):
+        print("Call endet")
+        return False
 
 
     def button_callback(self, button_str):
@@ -32,6 +53,7 @@ class Cellphone(Widget):
                 self.display_text = button_str 
             else:
                 self.display_text = self.display_text + button_str
+              
             # maximum_value = self.maximum_value
             # if maximum_value != None:
             #    if self.display_value > maximum_value: 
@@ -39,9 +61,14 @@ class Cellphone(Widget):
         elif button_str == 'del':
             self.display_text = self.display_text[:-1]
         elif button_str == 'dial':
-            pass
-        elif button_str == 'end':
-            pass
+            
+            dial_popup = DialPopup(self.display_text)
+            dial_popup.bind(on_open=self.start_call)
+            dial_popup.bind(on_dismiss=self.end_call)
+            dial_popup.open()
+    
+   
+
 
     
 
